988c2e873b5460bf91f27890812a578f6c21083f
Hirokazu Narui <hiro.narui@gmail.com>
2020-12-17 20:10:11 -0800

    modified csvloader test

--- test/models/history.scala
+++ test/models/history.scala
@@ -5,9 +5,8 @@ import java.time.LocalDateTime
 class CsvLoaderTest extends PlaySpec{
     "history" must{
         "return correct type of CsvLoader" in {
-            val data = CsvLoader.load("log.csv")
-            println(data.mkString(" "))
-            println(data.size)
+            val csv = getClass.getResource("read_test.csv").getPath
+            val data = CsvLoader.load(csv)
             data mustBe a[Map[LocalDateTime,Long]]
         }
     }
