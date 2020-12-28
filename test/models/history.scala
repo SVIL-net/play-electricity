d9568dc8764849026de0fac0bc275bb1daed89b7
kazuf3 <kazuhiro.funakoshi@furukawaelectric.com>
2020-12-28 10:38:25 -0800

    moved CSVLoader into History and refined its impl

--- test/models/history.scala
+++ test/models/history.scala
@@ -1,13 +1,25 @@
 import net.svil.bootcamp.electricity.Models._
 import org.scalatestplus.play._
 import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
-class CsvLoaderTest extends PlaySpec{
-    "history" must{
-        "return correct type of CsvLoader" in {
-            val csv = getClass.getResource("read_test.csv").getPath
-            val data = CsvLoader.load(csv)
-            data mustBe a[Map[LocalDateTime,Long]]
-        }
+class SimpleHistoryCSVReader extends PlaySpec{
+  "History" must {
+    val csv = getClass.getResource("read_test.csv").getPath
+    val result:History = History.fromCSV(csv)
+    val result_partial:Map[LocalDateTime,Long] =
+      Seq(
+        "2020-10-23T21:00:33",
+        "2020-10-25T13:00:33",
+        "2020-10-27T02:00:33",
+        "2020-10-28T09:00:33",
+        "2020-10-26T11:00:33",
+        "2020-10-25T08:00:33")
+        .map(LocalDateTime.parse(_))
+        .zip( Seq(13L,3L,2L,3L,11L,8L) ).toMap
+
+    "CSV can be read" in {
+      result_partial.to(Set).subsetOf(result.data.to(Set)) must be(true)
     }
+  }
 }
